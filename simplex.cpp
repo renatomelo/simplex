464a8f9f4502e6df9281f2d2e0097fe9a873a44d
rsmelo@inf.ufpr.br <rsmelo@inf.ufpr.br>
2017-06-15 21:02:29 -0300

    printing removed

--- simplex.cpp
+++ simplex.cpp
@@ -14,36 +14,7 @@ int igual(double x, double y) {
 	return fabs(x - y) < eps;
 }
 
-void mostra_A(double A[][MAX], int m, int n) {
-	int i, j;
-	printf("\nMatriz A: \n");
-	for (i = 1; i <= m; ++i) {
-		for (j = 1; j <= n; ++j) {
-			printf("%lf\t", A[i][j]);
-		}
-		printf("\n");
-	}
-}
-
-void mostra_z(double z[], int n) {
-	int i;
-	printf("\nCoeficientes de z: \n");
-	for (i = 1; i <= n; ++i) {
-		printf("%lf ", z[i]);
-	}
-	printf("\n");
-}
-
-void mostra_b(double b[], int m) {
-	int i, j;
-	printf("\nConteudo de b: \n");
-	for (i = 1; i <= m; ++i) {
-		printf("%lf ", b[i]);
-	}
-	printf("\n");
-}
-
-int base_viavel(double b[], int m) {
+int b_positivo (double b[], int m) {
 	int i, j;
 
 	for (i = 1; i <= m; ++i) {
@@ -105,7 +76,7 @@ int coluna_pivo(Tableau *tab) {
 	return -1;
 }
 
-//Encontra a linha pivô: menor razão p_i/x_v_i.
+//Encontra a linha pivô
 int linha_pivo(Tableau *tab, int col_pivo) {
 	double razao;
 	int i, lin_pivo;
@@ -114,17 +85,14 @@ int linha_pivo(Tableau *tab, int col_pivo) {
 	for (i = 1; i < tab->m; ++i) {
 		if (tab->A[i][col_pivo] - eps > 0) {
 			razao = tab->A[i][0] / tab->A[i][col_pivo];
-			printf("\nRazão: %lf", razao);
-			if (min >= razao) { //Em caso de empate, permanece o de menor índice
+			if (min >= razao) {
 				min = razao;
-				printf("\nMin: %lf", min);
 				lin_pivo = i;
 			}
 		}
 	}
 	//Se não tem elemento positivo na coluna pivô
-	if (min == INFINITY)
-		return -1;
+	if (min == INFINITY) return -1;
 
 	return lin_pivo;
 }
@@ -134,7 +102,6 @@ void pivoteamento(Tableau *tab, int lin_pivo, int col_pivo) {
 	int i, j;
 	double pivo;
 	pivo = tab->A[lin_pivo][col_pivo];
-	printf("\nPivô: %lf\n", pivo);
 
 	//Nova linha pivô
 	for (j = 0; j < tab->n; ++j) {
@@ -149,7 +116,6 @@ void pivoteamento(Tableau *tab, int lin_pivo, int col_pivo) {
 			}
 		}
 	}
-	mostra_tableau(tab);
 }
 void vetor_otimo(Tableau *tab, double x[]) {
 	int i, j, k = 1;
@@ -175,7 +141,7 @@ Tableau *auxiliar(Tableau *tab) {
 			aux->A[i][j] = tab->A[i][j];
 
 	for (i = 1; i < aux->m; ++i)
-		if (aux->A[i][0] < 0)
+		if (aux->A[i][0] + eps < 0)
 			for (j = 0; j < aux->n; ++j)
 				aux->A[i][j] = -aux->A[i][j];
 
@@ -194,19 +160,11 @@ int tem_matriz_identidade(Tableau *tab) {
 	int i, j;
 	for (i = 1; i < tab->m; i++) {
 		for (j = tab->n - tab->m + 1; j < tab->n; j++) {
-			printf("A[%d][%d] ", i + tab->n - tab->m, j);
-//			printf("%lf ",tab->A[i][j]);
-//			if(!igual(tab->A[i][j], 0)) printf("%lf ",tab->A[i][j]);
-//			if (!igual(fabs(tab->A[i][j]), 1) && !igual(tab->A[j][i+tab->n - tab->m], 0)){
-//				printf("[%lf %lf]",tab->A[i][j], tab->A[j][i+tab->n - tab->m]);
-//				return 0;
-//			}
 			if (i + tab->n - tab->m == j && !igual(fabs(tab->A[i][j]), 1))
 				return 0;
 			else if (i + tab->n - tab->m != j && !igual(tab->A[i][j], 0))
 				return 0;
 		}
-		printf("\n");
 	}
 	return 1;
 }
@@ -214,35 +172,18 @@ int tem_matriz_identidade(Tableau *tab) {
 int simplex(int m, int n, double z[], double A[][MAX], double b[], double *z0,
 		double x[]) {
 	int i, j, lin_pivo, col_pivo;
-//Atenção o simplex foi invocado e a entrada vai ser mostrada
-	/*	 printf("m = %d, n = %d \n",m,n);
-	 mostra_A(A, m, n);
-	 mostra_z(z, n);
-	 mostra_b(b, m);*/
 
-//TODO verificar se todas as colunas são linearmente independentes?
-//Controi o tableau
+	//Controi o tableau
 	Tableau *tab = cria_tableau(m, n, z, A, b);
-	mostra_tableau(tab);
-
-	if (tem_matriz_identidade(tab))
-		printf("\ntem matriz identidade\n");
-	else
-		printf("\nNÃOOOO TEM identidade\n");
 
 	if (!tem_matriz_identidade(tab)) {
 		// PROGRAMA AUXILIAR
 		Tableau *aux = auxiliar(tab);
-		mostra_tableau(aux);
-
 		add_folgas(aux);
-		printf("\nFolgas adicionadas\n");
-		mostra_tableau(aux);
-		//simplex
+
 		while (coluna_pivo(aux) > -1) {
-			//Acha a coluna pivô
 			col_pivo = coluna_pivo(aux);
-			lin_pivo = linha_pivo(aux, col_pivo); //Acha a linha pivô
+			lin_pivo = linha_pivo(aux, col_pivo);
 			if (lin_pivo >= 0)
 				pivoteamento(aux, lin_pivo, col_pivo); //Computa o novo tableau
 			else
@@ -251,31 +192,23 @@ int simplex(int m, int n, double z[], double A[][MAX], double b[], double *z0,
 
 		//Todos os valores na linha 0 são não-negativos
 		if (igual(aux->A[0][0], 0)) {
-			for (i = 1; i < tab->m; ++i) {
-				for (j = 0; j < tab->n; ++j) {
+			for (i = 1; i < tab->m; ++i)
+				for (j = 0; j < tab->n; ++j)
 					tab->A[i][j] = aux->A[i][j];
-				}
-			}
-			mostra_tableau(tab);
-		} else if (aux->A[0][0] > 0) {
+		} else if (aux->A[0][0] - eps > 0) {
 			return UNBD;
 		} else {
 			return NFEA;
 		}
-
 	}
 
-// A base é viável?
-	if (!base_viavel(b, m)) { // Não! vamos criar um programa auxiliar e resolvê-lo
-		printf("\nVamos precisar de um programa auxiliar!\n");
+	// A base é viável?
+	if (!b_positivo(b, m)) { // Não! vamos criar um programa auxiliar e resolvê-lo
 		Tableau *aux = auxiliar(tab);
-		mostra_tableau(aux);
 
-		//simplex
 		while (coluna_pivo(aux) > -1) {
-			//Acha a coluna pivô
 			col_pivo = coluna_pivo(aux);
-			lin_pivo = linha_pivo(aux, col_pivo); //Acha a linha pivô
+			lin_pivo = linha_pivo(aux, col_pivo);
 			if (lin_pivo >= 0)
 				pivoteamento(aux, lin_pivo, col_pivo); //Computa o novo tableau
 			else
@@ -284,38 +217,25 @@ int simplex(int m, int n, double z[], double A[][MAX], double b[], double *z0,
 
 		//Todos os valores na linha 0 são não-negativos
 		if (igual(aux->A[0][0], 0)) {
-			for (i = 1; i < tab->m; ++i) {
-				for (j = 0; j < tab->n; ++j) {
+			for (i = 1; i < tab->m; ++i)
+				for (j = 0; j < tab->n; ++j)
 					tab->A[i][j] = aux->A[i][j];
-				}
-			}
-			mostra_tableau(tab);
-		} else if (aux->A[0][0] > 0) {
+		} else if (aux->A[0][0] - eps > 0) {
 			return UNBD;
 		} else {
 			return NFEA;
 		}
+	}
 
-	}	// Deu boa, podemos fazer o pivoteamento
-	printf("\nA base é viável, mãos a obra!\n");
 	while (coluna_pivo(tab) > -1) {
-		//Acha a coluna pivô
-		col_pivo = coluna_pivo(tab);
+		col_pivo = coluna_pivo(tab); //Acha a coluna pivô
 		lin_pivo = linha_pivo(tab, col_pivo); //Acha a linha pivô
-		if (lin_pivo >= 0)
+		if (lin_pivo > -1)
 			pivoteamento(tab, lin_pivo, col_pivo); //Computa o novo tableau
 		else
 			return UNBD;
 	}
-//Todos os valores na linha 0 são não-negativos
-	if (m > n) {
-		int cont = 0;
-		for (i = 1; i < tab->m; ++i)
-			if (!igual(tab->A[i][0], 0))
-				cont++;
-		if (cont > n)
-			return NFEA;
-	}
+
 	*z0 = tab->A[0][0];
 	vetor_otimo(tab, x);
 	return FEA;
